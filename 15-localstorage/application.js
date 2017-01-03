20c86f90a3a3a0f2fc687fc23daf4378df2a0960
Jessica Koch <jessicakoch136@icloud.com>
2017-01-02 23:11:25 -0800

    update code to splice out elemtns

--- 15-localstorage/application.js
+++ 15-localstorage/application.js
@@ -1,6 +1,8 @@
 const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
   const items = JSON.parse(localStorage.getItem('items')) || [];
+const selectAllButton = document.querySelector('.selectAll');
+const deleteButton = document.querySelector('.delete');
 
 function addItem(e) {
     // prevent page from refreshing 
@@ -30,4 +32,40 @@ function populateList(plates = [], platesList) {
     }).join('');
 }
 
-  addItems.addEventListener('submit', addItem)
+function selectAll(e){
+    let checkboxes = itemsList.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = true;    
+    })
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+function deleteItem(e){
+    items.map(item => {
+        if (item.done) {
+            const index = items.indexOf(item);
+            if (index > -1) {
+                items.splice(index, 1);
+                localStorage.removeItem(item);
+            }
+            localStorage.setItem('items', JSON.stringify(items))
+            populateList(items, itemsList);
+            console.log(items)
+        }
+    })
+}
+
+deleteButton.addEventListener('click', deleteItem);
+selectAllButton.addEventListener('click', selectAll);
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
